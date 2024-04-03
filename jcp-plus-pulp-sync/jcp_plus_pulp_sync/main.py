7e68e6406c1902fdddc7dc0d35ba62410d1e40c8
Maruthi <>
2024-04-03 21:45:07 +0530

    Update main.py

--- jcp-plus-pulp-sync/jcp_plus_pulp_sync/main.py
+++ jcp-plus-pulp-sync/jcp_plus_pulp_sync/main.py
@@ -19,20 +19,17 @@ setup_logging(
 )
 
 logger.info("jcp-plus-pulp-sync started")
-print("logged")
 
 async def stuff():
     db_records = []
     try:
         filepath = str(get_data_dir("jcp-plus-pulp-server")) + "/peewee-sqlite.v2.db"
-        logger.info("jcp-plus-pulp-sync db filepath is " + str(filepath))
         con = sqlite3.connect(filepath)
         cur = con.cursor()
         cur.execute("SELECT * FROM eventmodel ORDER BY id ASC")
         db_records = cur.fetchall()
-        
     except Exception as err:
-        print('Query Error: ' + (str(err)))
+        logger.error('Fetch Query Error: ' + (str(err)))
     finally:
         con.close()
         try:
@@ -40,27 +37,28 @@ async def stuff():
                 response = requests.post('https://plus.tools.dp-dev.jcpcloud2.net/pulp?hostname=' + socket.gethostname() + '&username=' + os.getlogin(), data=json.dumps(db_records), timeout=300)
             else:
                 response = requests.get('https://plus.tools.dp-dev.jcpcloud2.net/pulp?hostname=' + socket.gethostname() + '&username=' + os.getlogin(), timeout=300)
-                if response.status_code == 200:
-                    response_data = response.json()
-                    print(response_data.status)
-                    if "delete_id" in response_data:
-                        try:
-                            con = sqlite3.connect(filepath)
-                            cur = con.cursor()
-                            cur.execute("DELETE FROM eventmodel WHERE id <= " + response_data.delete_id)
-                        except Exception as err:
-                            print('Query Error: ' + (str(err)))
-                        finally:
-                            con.close()
-                    if "notify" in response_data:
-                        notification = Notify()
-                        notification.application_name = response_data.notify.app_name
-                        notification.title = response_data.notify.title
-                        notification.message = response_data.notify.message
-                        notification.icon = str(get_data_dir("jcp-plus-pulp-qt")) + "/media/logo/logo.png"
-                        notification.urgency = "critical"
-                        notification.timeout = 10000  # 10 seconds
-                        notification.send()
+            logger.error(response.status_code)
+            if response.status_code == 200:
+                response_data = response.json()
+                print(response_data.status)
+                if "delete_id" in response_data:
+                    try:
+                        con = sqlite3.connect(filepath)
+                        cur = con.cursor()
+                        cur.execute("DELETE FROM eventmodel WHERE id <= " + response_data.delete_id)
+                    except Exception as err:
+                        logger.error('Delete Query Error: ' + (str(err)))
+                    finally:
+                        con.close()
+                if "notify" in response_data:
+                    notification = Notify()
+                    notification.application_name = response_data.notify.app_name
+                    notification.title = response_data.notify.title
+                    notification.message = response_data.notify.message
+                    notification.icon = str(get_data_dir("jcp-plus-pulp-qt")) + "/media/logo/logo.png"
+                    notification.urgency = "critical"
+                    notification.timeout = 10000  # 10 seconds
+                    notification.send()
         except Exception as err:
             print('Sync Error: ' + (str(err)))
     await asyncio.sleep(10)
