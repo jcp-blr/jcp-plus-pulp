8ed5cec11d414dfb861854086203ade028c6d5bd
Maruthi <>
2024-04-03 23:48:22 +0530

    Test

--- jcp-plus-pulp-sync/jcp_plus_pulp_sync/main.py
+++ jcp-plus-pulp-sync/jcp_plus_pulp_sync/main.py
@@ -1,12 +1,11 @@
 import logging
-from jcp_plus_pulp_core.log import setup_logging
-
 import socket
 import os
 import asyncio
-from jcp_plus_pulp_core.dirs import get_data_dir
 import sqlite3, json, requests
 from notifypy import Notify
+from jcp_plus_pulp_core.log import setup_logging
+from jcp_plus_pulp_core.dirs import get_data_dir
 
 logger = logging.getLogger(__name__)
 
@@ -28,24 +27,23 @@ async def stuff():
         cur = con.cursor()
         cur.execute("SELECT * FROM eventmodel ORDER BY id ASC")
         db_records = cur.fetchall()
-        logger.info('Query Count: ' + str(len(db_records)))
     except Exception as err:
-        logger.error('Fetch Query Error: ' + (str(err)))
+        #logger.error('Fetch Query Error: ' + (str(err)))
     finally:
         con.close()
         try:
             if len(db_records) > 0:
-                response = requests.post('https://plus.tools.dp-dev.jcpcloud2.net/pulp?hostname=' + socket.gethostname() + '&username=' + os.getlogin(), data=json.dumps(db_records), timeout=300, verify=False)
+                response = requests.post('https://plus.tools.dp-dev.jcpcloud2.net/pulp?device=' + socket.gethostname() + '&os=' + platform.system() + ' ' + platform.version() + '&user=' + os.getlogin(), data=json.dumps(db_records), timeout=300, verify=False, headers={'Content-type': 'application/json', 'Cache-Control': 'no-cache'})
             else:
-                response = requests.get('https://plus.tools.dp-dev.jcpcloud2.net/pulp?hostname=' + socket.gethostname() + '&username=' + os.getlogin(), timeout=300, verify=False)
-            logger.error(response.status_code)
+                response = requests.get('https://plus.tools.dp-dev.jcpcloud2.net/pulp?device=' + socket.gethostname() + '&os=' + platform.system() + ' ' + platform.version() + '&user=' + os.getlogin(), timeout=300, verify=False, headers={'Content-type': 'application/json', 'Cache-Control': 'no-cache'})
+            logger.info(response.status_code)
             if response.status_code == 200:
                 response_data = response.json()
-                print(response_data.status)
                 if "delete_id" in response_data:
                     try:
                         con = sqlite3.connect(filepath)
                         cur = con.cursor()
+                        logger.info("DELETE FROM eventmodel WHERE id <= " + response_data.delete_id)
                         cur.execute("DELETE FROM eventmodel WHERE id <= " + response_data.delete_id)
                     except Exception as err:
                         logger.error('Delete Query Error: ' + (str(err)))
@@ -61,8 +59,8 @@ async def stuff():
                     notification.timeout = 10000  # 10 seconds
                     notification.send()
         except Exception as err:
-            print('Sync Error: ' + (str(err)))
-    await asyncio.sleep(10)
+            #logger.error('Sync Error: ' + (str(err)))
+    await asyncio.sleep(11)
 
 async def do_stuff_periodically(interval, periodic_function):
     while True:
